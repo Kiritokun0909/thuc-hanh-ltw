7b7f07f65abc9b4de7489839a4fc0fc8061f6e1a
Kiritokun0909 <91424542+Kiritokun0909@users.noreply.github.com>
2025-05-09 09:45:07 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,40 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
+interface Product {
+  id: number,
+  name: string,
+  image: string
+}
+
 export default function Home() {
+  const [products, setProducts] = useState<[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('localhost:5001/products');
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+  
   return (
     <div className={styles.container}>
       <div className={styles.pageHeader}>
         <h1>Homepage</h1>
+      </div>
+
+      <div className={styles.productList}>
+        <ul>
+          {products.map((product: Product) =>(
+            <li>
+              {product.name}
+              <img src={product.image} alt={product.name}/>
+            </li>
+          )}
+        </ul>
       </div>
     </div>
   );
